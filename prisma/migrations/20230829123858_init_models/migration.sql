94729c3fb42df39b281e082f9cd755680aa150a2
João Rodrigues <joojrodrigues.dev@outlook.com>
2023-09-20 11:44:45 +0000

    feat: tables relations and migration

---
+++ prisma/migrations/20230829123858_init_models/migration.sql
@@ -0,0 +1,72 @@
+-- CreateEnum
+CREATE TYPE "PropertyType" AS ENUM ('RESIDENTIAL', 'CONDO');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('BUYER', 'REALTOR', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "homes" (
+    "id" UUID NOT NULL,
+    "address" VARCHAR(255) NOT NULL,
+    "city" VARCHAR(255) NOT NULL,
+    "listed_date" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "property_type" "PropertyType" NOT NULL,
+    "role" "Role" NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "realtor_id" UUID NOT NULL,
+
+    CONSTRAINT "homes_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "images" (
+    "id" UUID NOT NULL,
+    "url" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "home_id" UUID NOT NULL,
+
+    CONSTRAINT "images_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "phone" VARCHAR(30) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(30) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "messages" (
+    "id" UUID NOT NULL,
+    "message" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "home_id" UUID NOT NULL,
+    "realtor_id" UUID NOT NULL,
+    "buyer_id" UUID NOT NULL,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "homes" ADD CONSTRAINT "homes_realtor_id_fkey" FOREIGN KEY ("realtor_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "images" ADD CONSTRAINT "images_home_id_fkey" FOREIGN KEY ("home_id") REFERENCES "homes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_home_id_fkey" FOREIGN KEY ("home_id") REFERENCES "homes"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_realtor_id_fkey" FOREIGN KEY ("realtor_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_buyer_id_fkey" FOREIGN KEY ("buyer_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
