bc1a19703e9d9fa9b9fb00a668204e0eaf807833
João Rodrigues <joojrodrigues.dev@outlook.com>
2023-09-20 11:50:41 +0000

    feat: users dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
