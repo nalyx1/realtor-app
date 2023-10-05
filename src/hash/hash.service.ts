22212b44fc95309a44b7924cf9c1c87a26337dff
João Rodrigues <joojrodrigues.dev@outlook.com>
2023-10-05 12:16:28 +0000

    feat: hash with bcrypt service

---
+++ src/hash/hash.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class HashService {
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
+  async comparePassword(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
+}
